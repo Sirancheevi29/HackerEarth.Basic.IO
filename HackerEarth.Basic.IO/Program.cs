835d48ede960f390b83b86ffa86afc7d595a4930
sirancheevi <127745333+Sirancheevi29@users.noreply.github.com>
2023-08-13 19:00:19 +0200

    Add project files.

---
+++ HackerEarth.Basic.IO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int numberOfSongs = int.Parse(Console.ReadLine());
+List<int> Playlist = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
